d61fa2c45ba6c2e243fca4ae485ad47ab200a254
fairy-liyu <liyu_fairy@163.com>
2019-11-27 08:37:05 +0800

    修改boss页

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <el-row>
-      <el-button type="info" plain>信息按钮</el-button>
-      <el-button type="warning" plain>警告按钮</el-button>
-  </el-row>
+    <el-button type="primary" :loading="true">加载中</el-button>
   </div>
 </template>
