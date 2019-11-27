2c1d4a089ecf1bcf512d692d1804a39acb36a97d
utaokihj <hj528345347@163.com>
2019-11-27 09:45:05 +0800

    创建BossSystem.vue

---
+++ src/views/BossSystem.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="app-boss">
+        <div class="boss-header">
+            <h4>我是boss 页面</h4>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: "appBoss",
+    components: {
+
+    }
+}
+</script>
+
+<style lang="less" scoped>
+* {
+    margin: 0;
+    padding: 0;
+}
+.boss-header {
+    width: 100%;
+    height: 30px;
+    background: lightblue;
+}
+</style>
